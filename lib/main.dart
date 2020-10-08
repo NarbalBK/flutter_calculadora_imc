13979187e29eb5b19e3ceb142776c975a5c2b69a
Péricles Narbal <periclesnarbal@iMacdePericles.ciccr-ce.interno>
2020-10-08 16:34:20 -0300

    acrescenta o campo altura

--- lib/main.dart
+++ lib/main.dart
@@ -34,14 +34,31 @@ class _HomeState extends State<Home> {
             TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                  labelText: "Peso(kg)",
-                  labelStyle: TextStyle(color: Colors.green), 
+                labelText: "Peso(kg)",
+                labelStyle: TextStyle(color: Colors.green),
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura(cm)",
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            Container(
+              height: 50.0,
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text("Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                color: Colors.green,
+              ),
             )
           ],
-        )
-    );
+        ));
   }
 }
